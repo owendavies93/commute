caba646dceb519a38f7a3c0602e448379d714e87
Owen Davies <owen@odavi.es>
2015-06-19 15:54:12 +0100

    Add routes for starting and ending commutes. That was easy!

--- commute.pl
+++ commute.pl
@@ -28,6 +28,27 @@ get '/commutes/all' => sub {
     $c->render(json => $res);
 };
 
+post '/commutes/start' => sub {
+    my $c = shift;
+
+    my $status = $c->db->do(
+        'INSERT INTO commutes (start_time) VALUES (CURRENT_TIMESTAMP)');
+    return $c->render(json => { message => ($status ? 'Started' : 'Failed') });
+};
+
+post '/commutes/end' => sub {
+    my $c = shift;
+    my $mpg = $c->param('mpg');
+    my $len = $c->param('length');
+
+    my $status = $c->db->do(q(
+        UPDATE commutes SET end_time = NOW(), mpg = ?, length = ?,
+        total_time = TIMESTAMPDIFF(SECOND, start_time, NOW())
+        ORDER BY id DESC LIMIT 1
+    ), undef, $mpg, $len);
+    return $c->render(json => { message => ($status ? 'Ended' : 'Failed') });
+};
+
 app->secrets(['commutes rock']);
 app->start;
 
