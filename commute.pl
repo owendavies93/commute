00fa41c89c43a313515af3de63bcc35d59de780c
Owen Davies <owen@odavi.es>
2015-06-19 09:37:31 -0400

    Add db connection, all route and redirect

--- commute.pl
+++ commute.pl
@@ -1,6 +1,11 @@
 use Mojolicious::Lite;
 
 plugin 'basic_auth';
+plugin 'database', {
+    dsn      => 'dbi:mysql:commute:',
+    username => 'odavies',
+    password => 'testing123',
+};
 
 under sub {
     my $c = shift;
@@ -12,13 +17,24 @@ under sub {
     return undef;
 };
 
-get '/' => { json => { foo => [1, 'bar'] } };
+get '/' => sub { shift->render(template => 'index') };
 
-get '/commutes/' => { json => { foo => 'holla holla' } };
+get '/commutes' => sub { shift->redirect_to('/commute/commutes/all') };
 
+get '/commutes/all' => sub {
+    my $c = shift;
+    my $res = $c->db->selectall_arrayref(
+        'SELECT * FROM commutes', { Slice => {} });
+    $c->render(json => $res);
+};
+
+app->secrets(['commutes rock']);
 app->start;
 
 __DATA__
+
+@@ index.html.ep
+I'm doing regression analysis on my commute!
 
 @@ nope.html.ep
 Nope.
