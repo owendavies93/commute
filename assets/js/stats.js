5cd31c083466f0592bc3240d5545f269db2c2541
Owen Davies <owen@odavi.es>
2015-07-31 18:38:34 +0100

    Add inbound and outbound start time scatter plots

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -3,9 +3,17 @@ var parseDate = d3.time.format("%Y-%m-%d").parse;
 var dateFormatter = d3.time.format("%b %d");
 var timeFormatter = function(d) {
   var mins = Math.floor(d / 60);
+  mins = (mins < 10) ? '0' + mins : mins;
   var secs = d % 60;
   secs = (secs < 10) ? '0' + secs : secs;
   return mins + ":" + secs;
+}
+var startTimeFormatter = function(d) {
+  var hours = Math.floor(d / 60 / 60);
+  hours = (hours < 10) ? '0' + hours : hours;
+  var mins = Math.floor((d - (hours * 60 * 60)) / 60);
+  mins = (mins < 10) ? '0' + mins : mins;
+  return hours + ":" + mins;
 }
 
 function members_of_quantile_range(arr, x, y) {
@@ -22,11 +30,19 @@ function members_of_quantile_range(arr, x, y) {
   return res;
 }
 
+function buildBaseChart(height, width, margins, container) {
+  return d3.select(container).append("svg")
+          .attr("width", width + margins.left + margins.right)
+          .attr("height", height + margins.top + margins.bottom)
+          .append("g")
+          .attr("transform", "translate(" + margins.left + "," + margins.top + ")");
+}
+
 function buildBarChart(height, width, margins, container) {
   var x = d3.time.scale().range([0, width]);
   var y = d3.scale.linear().range([height, 0]);
 
-  var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(10).tickFormat(dateFormatter);
+  var xAxis = d3.svg.axis().scale(x).orient("bottom").tickFormat(dateFormatter);
   var yAxis = d3.svg.axis().scale(y).orient("left")  .tickFormat(timeFormatter);
 
   var tip = d3.tip()
@@ -36,11 +52,7 @@ function buildBarChart(height, width, margins, container) {
                 return "Date: <strong>" + dateFormatter(d.date) + "</strong><br>Total Time: <strong>" + timeFormatter(d.total_time) + "</strong><br>Direction: <strong>" + d.direction + "</strong>";
               });
 
-  var svg = d3.select(container).append("svg")
-              .attr("width", width + margins.left + margins.right)
-              .attr("height", height + margins.top + margins.bottom)
-              .append("g")
-              .attr("transform", "translate(" + margins.left + "," + margins.top + ")");
+  var svg = buildBaseChart(height, width, margins, container);
 
   svg.call(tip);
   return {
@@ -50,6 +62,24 @@ function buildBarChart(height, width, margins, container) {
     xAxis: xAxis,
     yAxis: yAxis,
     tip:   tip
+  };
+}
+
+function buildScatterChart(height, width, margins, container) {
+  var x = d3.scale.linear().range([0, width]);
+  var y = d3.scale.linear().range([height, 0]);
+
+  var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(5).tickFormat(startTimeFormatter);
+  var yAxis = d3.svg.axis().scale(y).orient("left").tickFormat(timeFormatter);
+
+  var svg = buildBaseChart(height, width, margins, container);
+
+  return {
+    svg:   svg,
+    x:     x,
+    y:     y,
+    xAxis: xAxis,
+    yAxis: yAxis
   };
 }
 
@@ -126,6 +156,46 @@ function populateBarChart(chart, data, height, width, min, max) {
       .attr("stroke-dasharray", "10, 5");
 }
 
+function populateScatterChart(chart, data, height, width, min, max) {
+  var minStart = d3.min(data.map(function(d) { return d.start_time }));
+  var maxStart = d3.max(data.map(function(d) { return d.start_time }));
+  var diff     = ((maxStart - minStart) * 0.1);
+
+  chart.x.domain([minStart - diff, maxStart + diff]);
+  chart.y.domain([min - 60, max + 60]);
+
+  chart.xAxis.tickValues(d3.range(minStart, maxStart + 150, diff));
+  chart.yAxis.tickValues(d3.range(Math.floor(min / 600) * 600, max, 600));
+
+  chart.svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(chart.xAxis)
+      .append("text")
+      .attr("x", width)
+      .attr("y", -6)
+      .style("text-anchor", "end")
+      .text("Start Time");
+
+  chart.svg.append("g")
+      .attr("class", "y axis")
+      .call(chart.yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Total Time (mins:secs)");
+
+  chart.svg.selectAll(".dot")
+      .data(data)
+      .enter().append("circle")
+      .attr("class", "dot")
+      .attr("r", 2)
+      .attr("cx", function(d) { return chart.x(d.start_time); })
+      .attr("cy", function(d) { return chart.y(d.total_time); });
+}
+
 function populateStats(container, data) {
   var times = data.map(function(d) { return d.total_time });
 
@@ -156,14 +226,20 @@ $(document).ready(function() {
   var inbound_svg  = buildBarChart((3 * height) / 4, width / 2 + 50, small_margins, "#inbound-graph-cont");
   var outbound_svg = buildBarChart((3 * height) / 4, width / 2 + 50, small_margins, "#outbound-graph-cont");
 
+  var i_scatter_svg = buildScatterChart((3 * height) / 4, width / 2 + 50, small_margins, "#inbound-scatter-cont");
+  var o_scatter_svg = buildScatterChart((3 * height) / 4, width / 2 + 50, small_margins, "#outbound-scatter-cont");
+
   d3.json('/commute/commutes/all', function(error, data) {
     if (error) throw error;
 
     var min = Number.MAX_VALUE;
     var max = 0;
     data.forEach(function(d) {
-      d.date = parseDate(d.date);
+      d.date       = parseDate(d.date);
       d.total_time = +d.total_time;
+
+      d.start_date = new Date(d.start_time);
+      d.start_time = (d.start_date.getTime() - d.start_date.setHours(0, 0, 0, 0)) / 1000 ;
 
       if (d.total_time < min) {
         min = d.total_time;
@@ -186,5 +262,8 @@ $(document).ready(function() {
     });
     populateBarChart(outbound_svg, outbound, (3 * height) / 4, width / 2 + 50, min, max);
     populateStats($(".outbound-stats"), outbound);
+
+    populateScatterChart(i_scatter_svg, inbound, (3 * height) / 4, width / 2 + 50, min, max);
+    populateScatterChart(o_scatter_svg, outbound, (3 * height) / 4, width / 2 + 50, min, max);
   });
 });
