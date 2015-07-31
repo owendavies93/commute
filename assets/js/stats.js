a344a40f94813823e8bedca1fdb334264dc38d27
Owen Davies <owen@odavi.es>
2015-07-31 09:14:50 +0100

    Add inbound and outbound graphs
    Extract out much of the graph logic into functions to do this

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,6 +1,3 @@
-var margins   = { top: 40, right: 30, bottom: 70, left: 60 };
-var width     = 850 - margins.left - margins.right;
-var height    = 500 - margins.top - margins.bottom;
 var parseDate = d3.time.format("%Y-%m-%d").parse;
 
 var dateFormatter = d3.time.format("%b %d");
@@ -22,11 +19,10 @@ function members_of_quantile_range(arr, x, y) {
       res.push(m);
     }
   }
-  console.log(res);
   return res;
 }
 
-function buildMainChart() {
+function buildBarChart(height, width, margins, container) {
   var x = d3.time.scale().range([0, width]);
   var y = d3.scale.linear().range([height, 0]);
 
@@ -49,109 +45,153 @@ function buildMainChart() {
                 return "Date: <strong>" + dateFormatter(d.date) + "</strong><br>Total Time: <strong>" + timeFormatter(d.total_time) + "</strong><br>Direction: <strong>" + d.direction + "</strong>";
               });
 
-  var svg = d3.select("#main-graph-container").append("svg")
+  var svg = d3.select(container).append("svg")
               .attr("width", width + margins.left + margins.right)
               .attr("height", height + margins.top + margins.bottom)
               .append("g")
               .attr("transform", "translate(" + margins.left + "," + margins.top + ")");
 
   svg.call(tip);
+  return {
+    svg:   svg,
+    x:     x,
+    y:     y,
+    xAxis: xAxis,
+    yAxis: yAxis,
+    tip:   tip
+  };
+}
+
+function populateBarChart(chart, data, height, width, min, max) {
+  chart.x.domain(d3.extent(data, function(d) { return d.date }));
+  chart.y.domain([min - 500, max]);
+
+  chart.svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(chart.xAxis);
+
+  chart.svg.append("g")
+      .attr("class", "y axis")
+      .call(chart.yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Total Time (mins:secs)");
+
+  chart.svg.selectAll(".bar")
+      .data(data)
+      .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d, i) { return i * (width / data.length) + 5; })
+      .attr("width", (width / data.length) - 5)
+      .attr("y", function(d) { return chart.y(d.total_time); })
+      .attr("height", function(d) { return height - chart.y(d.total_time); })
+      .on('mouseover', chart.tip.show)
+      .on('mouseout', chart.tip.hide);
+
+  var times = data.map(function(d) { return d.total_time });
+  var avg = ss.mean(times);
+  var lowerQ = ss.quantile(times, 0.25);
+  var upperQ = ss.quantile(times, 0.75);
+
+  function straightLineGenerator(d, i) {
+    if (i == 0) return 0;
+    if (i == data.length - 1) return i * (width / data.length) + (width / data.length);
+    return i * (width / data.length) + (width / data.length) / 2;
+  }
+
+  var avgLine = d3.svg.line()
+                  .x(straightLineGenerator)
+                  .y(function(d, i) { return chart.y(avg); });
+
+  var lQLine = d3.svg.line()
+                  .x(straightLineGenerator)
+                  .y(function(d, i) { return chart.y(lowerQ); });
+
+  var uQLine = d3.svg.line()
+                  .x(straightLineGenerator)
+                  .y(function(d, i) { return chart.y(upperQ); });
+
+  chart.svg.append("path")
+      .attr("d", avgLine(data))
+      .attr("stroke", "darkgreen")
+      .attr("stroke-width", 2);
+
+  chart.svg.append("path")
+      .attr("d", lQLine(data))
+      .attr("stroke", "#555")
+      .attr("stroke-width", 1)
+      .attr("stroke-dasharray", "10, 5");
+
+  chart.svg.append("path")
+      .attr("d", uQLine(data))
+      .attr("stroke", "#555")
+      .attr("stroke-width", 1)
+      .attr("stroke-dasharray", "10, 5");
+}
+
+function populateStats(container, data) {
+  var times = data.map(function(d) { return d.total_time });
+
+  var total  = data.length;
+  var avg    = ss.mean(times);
+  var median = timeFormatter(Math.round(ss.median(times)));
+  var stddev = ss.standard_deviation(times).toFixed(2);
+  var range  = ss.max(times) - ss.min(times);
+  var iqr    = ss.iqr(times);
+  var iqa    = timeFormatter(Math.round(ss.mean(members_of_quantile_range(times, 0.25, 0.75))));
+
+  $('.stat-total', container).html(total);
+  $('.stat-avg', container)  .html(timeFormatter(Math.round(avg)));
+  $('.stat-med', container)  .html(median);
+  $('.stat-sd', container)   .html(stddev + " secs");
+  $('.stat-range', container).html(range + " secs");
+  $('.stat-iqr', container)  .html(iqr + " secs");
+  $('.stat-iqa', container)  .html(iqa);
+}
+
+$(document).ready(function() {
+  var big_margins   = { top: 40, right: 30, bottom: 70, left: 60 };
+  var small_margins = { top: 20, right: 15, bottom: 30, left: 60 };
+  var width         = 850 - big_margins.left - big_margins.right;
+  var height        = 500 - big_margins.top - big_margins.bottom;
+
+  var bidir_svg    = buildBarChart(height, width, big_margins, "#bidir-graph-cont");
+  var inbound_svg  = buildBarChart((3 * height) / 4, width / 2, small_margins, "#inbound-graph-cont");
+  var outbound_svg = buildBarChart((3 * height) / 4, width / 2, small_margins, "#outbound-graph-cont");
 
   d3.json('/commute/commutes/all', function(error, data) {
     if (error) throw error;
 
+    var min = Number.MAX_VALUE;
+    var max = 0;
     data.forEach(function(d) {
       d.date = parseDate(d.date);
       d.total_time = +d.total_time;
+
+      if (d.total_time < min) {
+        min = d.total_time;
+      } else if (d.total_time > max) {
+        max = d.total_time;
+      }
     });
 
-    x.domain(d3.extent(data, function(d) { return d.date }));
-    y.domain([d3.min(data, function(d) { return d.total_time }) - 500,
-              d3.max(data, function(d) { return d.total_time })]);
+    populateBarChart(bidir_svg, data, height, width, min, max);
+    populateStats($(".stats-summary"), data);
 
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+    var inbound = data.filter(function(v) {
+      return v.direction === 'in';
+    });
+    populateBarChart(inbound_svg, inbound, (3 * height) / 4, width / 2, min, max);
+    populateStats($(".inbound-stats"), inbound);
 
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Total Time (mins:secs)");
-
-    svg.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d, i) { return i * (width / data.length) + 5; })
-        .attr("width", (width / data.length) - 5)
-        .attr("y", function(d) { return y(d.total_time); })
-        .attr("height", function(d) { return height - y(d.total_time); })
-        .on('mouseover', tip.show)
-        .on('mouseout', tip.hide);
-
-    var times = data.map(function(d) { return d.total_time });
-    var avg = ss.mean(times);
-    var lowerQ = ss.quantile(times, 0.25);
-    var upperQ = ss.quantile(times, 0.75);
-
-    function straightLineGenerator(d, i) {
-      if (i == 0) return 0;
-      if (i == data.length - 1) return i * (width / data.length) + (width / data.length);
-      return i * (width / data.length) + (width / data.length) / 2;
-    }
-
-    var avgLine = d3.svg.line()
-                    .x(straightLineGenerator)
-                    .y(function(d, i) { return y(avg); });
-
-    var lQLine = d3.svg.line()
-                    .x(straightLineGenerator)
-                    .y(function(d, i) { return y(lowerQ); });
-
-    var uQLine = d3.svg.line()
-                    .x(straightLineGenerator)
-                    .y(function(d, i) { return y(upperQ); });
-
-    svg.append("path")
-        .attr("d", avgLine(data))
-        .attr("stroke", "darkgreen")
-        .attr("stroke-width", 2);
-
-    svg.append("path")
-        .attr("d", lQLine(data))
-        .attr("stroke", "#555")
-        .attr("stroke-width", 1)
-        .attr("stroke-dasharray", "10, 5");
-
-    svg.append("path")
-        .attr("d", uQLine(data))
-        .attr("stroke", "#555")
-        .attr("stroke-width", 1)
-        .attr("stroke-dasharray", "10, 5");
-
-    var total  = data.length;
-    var median = timeFormatter(Math.round(ss.median(times)));
-    var stddev = ss.standard_deviation(times).toFixed(2);
-    var range  = ss.max(times) - ss.min(times);
-    var iqr    = ss.iqr(times);
-    var iqa    = timeFormatter(Math.round(ss.mean(members_of_quantile_range(times, 0.25, 0.75))));
-
-    $('#stat-total').html(total);
-    $('#stat-avg').html(timeFormatter(Math.round(avg)));
-    $('#stat-med').html(median);
-    $('#stat-sd').html(stddev + " secs");
-    $('#stat-range').html(range + " secs");
-    $('#stat-iqr').html(iqr + " secs");
-    $('#stat-iqa').html(iqa);
+    var outbound = data.filter(function(v) {
+      return v.direction === 'out';
+    });
+    populateBarChart(outbound_svg, outbound, (3 * height) / 4, width / 2, min, max);
+    populateStats($(".outbound-stats"), outbound);
   });
-}
-
-$(document).ready(function() {
-    buildMainChart();
 });
